d21eab612e9dc344ff03ffd305d77db2eabf438c
philopian <philopian@gmail.com>
2025-03-09 14:13:15 -0700

    feat: server - update deno.json and deno.lock for new dependencies; implement Zod validation in user routes

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,19 @@
 import { Hono } from 'hono'
 import { cors } from 'hono/cors'
+import { z } from 'zod'
+import { zValidator } from '@hono/zod-validator'
+
+const UserSchema = z.object({
+  name: z.string(),
+  age: z.number(),
+})
 
 export function createApp() {
   const usersRouter = new Hono()
     .get('/', (c) => c.text('Hello Users!'))
     .get('/:id', (c) => c.text(`Hello User ${c.req.param('id')}!`))
-    .post('/', (c) => c.text('Create User'))
-    .put('/:id', (c) => c.text(`Update User ${c.req.param('id')}!`))
+    .post('/', zValidator('json', UserSchema), (c) => c.text('Create User'))
+    .put('/:id', zValidator('json', UserSchema), (c) => c.text(`Update User ${c.req.param('id')}!`))
     .delete('/:id', (c) => c.text(`Delete User ${c.req.param('id')}!`))
 
   const todosRouter = new Hono()
